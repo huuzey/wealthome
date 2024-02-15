324b78a4c79eb9e5426df3b9930ba4fec2e93b7b
huuzey <weleladinsefa@gmail.com>
2024-02-15 20:05:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// navbar toggle in mobile
+const /** {node element} */ $navbar = document.querySelector("[data-navbar]");
+const /** {node element} */ $navToggler =
+    document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+// header scroll state
+
+const /** {node element} */ $header = document.querySelector("[data-header]");
+window.addEventListener("scroll", (e) => {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+// add to favorite
+const /**{nodelist} */ $toggleBtns =
+    document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach(($toggleBtn) => {
+  $toggleBtn.addEventListener("click", () => {
+    $toggleBtn.classList.toggle("active");
+  });
+});
